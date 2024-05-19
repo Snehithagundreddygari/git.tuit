d1eacd7fed0b2811975adf44438f61066a7b15d4
snehi <snehithagundreddygari@gmail.com>
2024-05-19 13:58:37 +0530

    commiting

---
+++ nQueens.java
@@ -0,0 +1,58 @@
+public class nQueens {
+    public static void main(String args[]){
+        int n=3;
+        char board[][]=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+            System.out.println();
+        }
+        nQueens(board,0);
+    }
+    public static boolean isSafe(char board[][],int row,int col){
+        //vertical
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        //left diagnol
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        //right 
+        for(int i=row-1,j=col+1;i>=0 && j<board.length;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void nQueens(char board[][],int row){
+        if(row==board.length){
+            print(board);
+            return;
+        }
+        for(int j=0;j<board.length;j++){
+            if(isSafe(board,row,j)){
+                board[row][j]='Q';
+                nQueens(board,row+1);//recursive call for the col
+                board[row][j]='.'; 
+            }
+        }
+    }
+    public static void print(char board[][]){
+        System.out.println("------chessBoard------");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        //System.out.println();
+    }
+    
+}
